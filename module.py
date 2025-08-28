b7043947c30294134cd25dbdcab34f598cda667d
TanushTiwari <dr.amritkumar.maa.janki@gmail.com>
2025-08-28 21:55:59 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,12 @@
+import pyjokes
+
+# print("Printing Joke...")
+joke = pyjokes.get_joke()
+print(joke)
+
+# Keep the console window open
+input("\nPress Enter to exit...")
+
+"""
+Here CH 1 ends !!?
+"""
